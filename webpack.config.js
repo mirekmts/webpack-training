0c57c54774e50740785e4baaed6a2537b74b01be
mirekmts <mirekmts@gmail.com>
2018-07-05 21:46:07 +0200

    Add react, babel and eslint. Finish configuration for simple react project

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,32 @@
+const path = require('path');
+
 module.exports = {
     entry: [
-        './src/index.js'
+        './src/index.js',
     ],
     output: {
-        path: __dirname + '/dist',
+        path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        filename: 'bundle.js'
+        filename: 'bundle.js',
     },
     devServer: {
-        contentBase: './dist'
-    }
-}
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['eslint-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+};
